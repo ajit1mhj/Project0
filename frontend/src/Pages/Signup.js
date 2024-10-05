6192349e8aa222d5fae36b0b2a0fc621ac1718ac
ajit maharjan <ajit1mhj@gmail.com>
2024-10-05 23:36:43 +0545

    fontend change 2

--- frontend/src/Pages/Signup.js
+++ frontend/src/Pages/Signup.js
@@ -1,22 +1,61 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import {ToastContainer} from 'react-toastify'
+import { handleError, handleSuccess } from '../utils'
+import 'react-toastify/dist/ReactToastify.css';
+
 function Signup() {
-    const [loginInfo,setLoginInfo]=useState({
+    const [signupInfo,setSignupInfo]=useState({
         name:'',
         email:'',
         password:''
     })
+    const navigate = useNavigate();
     const handleChange = (e)=>{
         const{name,value}=e.target;
         console.log(name,value);
-        const copyy
+        const copySignupInfo = {...signupInfo};
+        copySignupInfo[name] = value;
+        setSignupInfo(copySignupInfo);
+        }
         
+    const handleSignup =async (e) =>{
+      e.preventDefault();
+      const {name,email,password}=signupInfo;
+      if (!name || !email || !password){
+        return handleError('name,email, password are required')
+      }
+      try{
+        const url = "http://localhost:8080/auth/signup";
+        const response = await fetch(url,{
+          method: "POST",
+          headers:{
+            'Content-Type':'application/json'
+          },
+          body:JSON.stringify(signupInfo)
+        });
+        const result = await response.json();
+        const {success,message,error}= result;
+        if (success){
+          handleSuccess(message);
+          setTimeout(()=>{
+            navigate('/login')
+          },1000)
+        }else if(error){
+          const details = error?.details[0].message;
+          handleError(details);
+        }else if(!success){
+          handleError(message);
+        }
+        console.log(result);
+      } catch(err){
+        handleError(err);
+      }
     }
   return (
     <div className='container'>
         <h1>Sign Up</h1>
-        <form>
+        <form onSubmit={handleSignup}>
             <div>
               <label htmlFor='name'>Name</label>   
               <input 
@@ -24,7 +63,8 @@ function Signup() {
               type="text"
               name="name"
               autoFocus
-              placeholder='Enter Your name..'
+              placeholder='Enter Your name...'
+              value={signupInfo.name}
               />
               </div> 
               <div>
@@ -35,6 +75,7 @@ function Signup() {
               name="email"
               
               placeholder='Enter Your email..'
+              value={signupInfo.email}
               />
              </div>
              <div>
@@ -45,10 +86,11 @@ function Signup() {
               name="password"
               
               placeholder='Enter Your password..'
+              value={signupInfo.password}
               />
 
             </div>
-            <button>Signup</button>
+            <button type='submit'>Signup</button>
             <span>Already have an Account ?
                 <Link to="/login">Login</Link>
             </span>
@@ -56,6 +98,6 @@ function Signup() {
         <ToastContainer/>
     </div>
   )
-}
 
+}
 export default Signup
